6db0eadf490382b17b9f369376c5646d456cd8c2
gundamMC <gundammc@qq.com>
2017-08-03 23:21:19 +0800

    Version info & Start button

--- EndlessLauncher/Properties/Resources.Designer.cs
+++ EndlessLauncher/Properties/Resources.Designer.cs
@@ -129,5 +129,35 @@ namespace EndlessLauncher.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap StartGame {
+            get {
+                object obj = ResourceManager.GetObject("StartGame", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap StartGame_Hover {
+            get {
+                object obj = ResourceManager.GetObject("StartGame_Hover", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap StartGame_Pressed {
+            get {
+                object obj = ResourceManager.GetObject("StartGame_Pressed", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
